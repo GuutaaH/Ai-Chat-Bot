a0c9376508c1031363113b9b4a11b5170d38ba37
GuutaaH <63647222+GuutaaH@users.noreply.github.com>
2025-03-27 23:20:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-import disableReactDevtools from "@fvilers/disable-react-devtools";
+import disableReactDevtools from "@fvilers/disable-react-devtools";  // Default import
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevtools();  // Call it here in production
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
