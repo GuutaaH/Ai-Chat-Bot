ab1027e13c48cce6f9b0ad7167bc4700a64edb0c
GuutaaH <63647222+GuutaaH@users.noreply.github.com>
2025-03-27 23:39:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevtools();  // Disable React DevTools in production
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
